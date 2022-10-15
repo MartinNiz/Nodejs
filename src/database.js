e1893a5b3b3da1702fdb74b120f3bb4f533997a3
MartinNiz <martin.niz.niz@gmail.com>
2022-10-15 12:15:54 -0300

    generar notas y mostrarlas

--- src/database.js
+++ src/database.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/notes-db-app')
+mongoose.connect('mongodb://localhost/notes-db-app', {
+  family: 4 // Use IPv4, skip trying IPv6
+})
 .then(db => console.log('DB is conected'))
 .catch(err => console.log(err));
--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,7 +1,39 @@
 const router = require('express').Router();
 
-router.get('/notes', (req,res) => {
-  res.send('Notes');
+const Note = require('../models/Note');
+
+router.get('/notes/add', (req, res) => {
+  res.render('notes/add-notes');
+});
+
+router.post('/notes/add-notes', async (req, res) => {
+  const { title, description } = req.body;
+  const errors = [];
+  
+  if (!title) { 
+    errors.push({text: 'Please add a title'})
+  }
+  
+  if (!description) {
+    errors.push({text: 'Please add a description'})
+  }
+
+  if (errors.length > 0) {
+    res.render('notes/add-notes', {
+      errors,
+      title,
+      description
+    })
+  } else{
+    const newNote = new Note({title, description});
+    await newNote.save();
+    res.redirect('/notes');
+  }
+});
+
+router.get('/notes', async (req, res) => {
+  const notes = await Note.find().lean();
+  res.render('notes/all-notes', { notes });
 });
 
 module.exports = router;
