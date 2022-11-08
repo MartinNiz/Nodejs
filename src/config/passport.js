33a31f32f496c5068329894d16391862fbfd9c2c
MartinNiz <martin.niz.niz@gmail.com>
2022-11-07 21:24:21 -0300

    sign in with passport

--- src/config/passport.js
+++ src/config/passport.js
@@ -3,29 +3,35 @@ const LocalStrategy = require('passport-local');
 
 const User = require('../models/Users')
 
-passport.use(new LocalStrategy({
-  usernameField: 'email'
-}, async (email, password, done) => {
-  const user = await User.findOne({email: email});
-  if (!user) {
-    return done(null, false, {message: 'Not User found.'});
-  } else {
-    const match = await User.matchPassword({password});
-    if (match) {
-      return done(null, user)
-    } else {
-      return done(null, false, {message: 'Incorret Password.'});
-    }
-  }
-}));
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+    },
+    async (email, password, done) => {
+      // Match Email's User
+      const user = await User.findOne({ email: email });
 
+      if (!user) {
+        return done(null, false, { message: "Not User found." });
+      }
+
+      // Match Password's User
+      const isMatch = await user.matchPassword(password);
+      if (!isMatch)
+        return done(null, false, { message: "Incorrect Password." });
+      
+      return done(null, user);
+    }
+  )
+);
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-  User.findByID(id, (err, user) => {
+  User.findById(id, (err, user) => {
     done(err, user);
   });
-})
+});
